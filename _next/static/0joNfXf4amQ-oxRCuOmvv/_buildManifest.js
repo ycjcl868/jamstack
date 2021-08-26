c25013652f0dc48b41f723efe5391062be01e287
ycjcl868 <ycjcl868@users.noreply.github.com>
2021-08-26 08:14:51 +0000

    deploy: f4d22fb8eb72550c60943bb4bd4c00f6ee699428

--- _next/static/0joNfXf4amQ-oxRCuOmvv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/38de2d2d6148195b16a7.css","static/chunks/pages/index-f8f41f7a53cb0dce48a7.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/yFIUZamkK-V7RmaABUTcy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e6e91d9e44ba21119b05.css","static/chunks/pages/index-60f85767059e2fb49411.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
