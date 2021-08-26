b972b68a09362c3466868cb59555a7a8f6f1a3f1
ycjcl868 <chaolinjin@gmail.com>
2021-08-26 15:37:08 +0800

    chore: ts error

--- corejs-plugin.ts
+++
@@ -1,8 +0,0 @@
-import { IApi } from 'umi';
-
-export default (api: IApi) => {
-  api.modifyBabelPresetOpts(memo => {
-    memo.env!.useBuiltIns = 'usage';
-    return memo;
-  });
-};
--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { readFileSync, existsSync } from 'fs';
-import { join } from 'path';
-
-test('home page', () => {
-  const htmlPath = join(__dirname, '..', 'dist', 'index.html');
-  expect(existsSync(htmlPath)).toBeTruthy();
-  const indexHTML = readFileSync(htmlPath, 'utf-8');
-  expect(indexHTML).toMatch('<article');
-});
